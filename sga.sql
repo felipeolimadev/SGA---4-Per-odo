54bc92052b96aeb195f2732b5c244a663f96fda9
felipeolimadev <felipeolimadev@gmail.com>
2018-11-07 16:40:05 -0200

    Incluido banco de dados

---
+++ sga.sql
@@ -0,0 +1,202 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 07-Nov-2018 às 19:39
+-- Versão do servidor: 10.1.36-MariaDB
+-- versão do PHP: 7.2.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sga`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `administrar`
+--
+
+CREATE TABLE `administrar` (
+  `cod_admin` int(11) NOT NULL,
+  `nome` varchar(50) NOT NULL,
+  `login` varchar(20) NOT NULL,
+  `senha` varchar(40) NOT NULL,
+  `status` enum('0','1') NOT NULL DEFAULT '1'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `aluno`
+--
+
+CREATE TABLE `aluno` (
+  `cod_aluno` int(11) NOT NULL,
+  `matricula` varchar(10) NOT NULL,
+  `nome` varchar(50) NOT NULL,
+  `telefone` varchar(14) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `aluno_turma`
+--
+
+CREATE TABLE `aluno_turma` (
+  `cod_aluno` int(11) NOT NULL,
+  `cod_turma` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `curso`
+--
+
+CREATE TABLE `curso` (
+  `cod_curso` int(11) NOT NULL,
+  `nome` varchar(30) NOT NULL,
+  `sigla` varchar(4) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `curso`
+--
+
+INSERT INTO `curso` (`cod_curso`, `nome`, `sigla`) VALUES
+(1, 'Sistemas de Informacao', 'SI'),
+(2, 'Agronomia', 'AG');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `login`
+--
+
+CREATE TABLE `login` (
+  `id` int(11) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `code` varchar(255) NOT NULL,
+  `senha` varchar(255) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `painel` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `login`
+--
+
+INSERT INTO `login` (`id`, `status`, `code`, `senha`, `nome`, `painel`) VALUES
+(11, 'Ativo', 'manutencao@ipca.br', 'manutencao', 'Manutenção de Sistemas', 'secretaria'),
+(12, 'Ativo', '1111549@edu.ipca.br', '1111549', 'Danilo Fernandes da Silva', 'aluno'),
+(15, 'Ativo', 'coordenacao@ipca.br', 'coordenacao', 'Felipe Lima', 'secretaria'),
+(16, 'Ativo', 'professor@edu.ipca.br', 'educacao', 'Professor Regente de Turma', 'professor'),
+(17, 'Ativo', 'educacao@edu.ipca.br', 'educacao', 'Inst.de Pesquisa e Centro de Aval. de Software', 'secretaria');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `turma`
+--
+
+CREATE TABLE `turma` (
+  `cod_turma` int(11) NOT NULL,
+  `letra` varchar(1) NOT NULL,
+  `periodo` int(11) NOT NULL,
+  `cod_curso` int(11) NOT NULL,
+  `ano` int(11) NOT NULL,
+  `semestre` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `turma`
+--
+
+INSERT INTO `turma` (`cod_turma`, `letra`, `periodo`, `cod_curso`, `ano`, `semestre`) VALUES
+(1, 'A', 2, 1, 2017, 4);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `administrar`
+--
+ALTER TABLE `administrar`
+  ADD PRIMARY KEY (`cod_admin`);
+
+--
+-- Indexes for table `aluno`
+--
+ALTER TABLE `aluno`
+  ADD PRIMARY KEY (`cod_aluno`);
+
+--
+-- Indexes for table `curso`
+--
+ALTER TABLE `curso`
+  ADD PRIMARY KEY (`cod_curso`);
+
+--
+-- Indexes for table `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `turma`
+--
+ALTER TABLE `turma`
+  ADD PRIMARY KEY (`cod_turma`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `administrar`
+--
+ALTER TABLE `administrar`
+  MODIFY `cod_admin` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `aluno`
+--
+ALTER TABLE `aluno`
+  MODIFY `cod_aluno` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `curso`
+--
+ALTER TABLE `curso`
+  MODIFY `cod_curso` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `login`
+--
+ALTER TABLE `login`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT for table `turma`
+--
+ALTER TABLE `turma`
+  MODIFY `cod_turma` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
